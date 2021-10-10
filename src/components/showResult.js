bbedb3f60c8de1dd2e4184845d55bdd50e73e6ba
Leandro <leandro.martins1912@gmail.com>
2021-10-09 22:56:34 -0300

    Changed ShowResult.js

--- src/components/showResult.js
+++ src/components/showResult.js
@@ -1,14 +1,16 @@
 import React from "react";
 
+import "./showResult.css"
+
 function ShowResult(props) {
   const { numero_digitado, divisores, numeros_primos_divisores } = props.result;
   return (
-    <div>
+    <div >
       {numero_digitado && divisores && numeros_primos_divisores ? (
-        <div>
-          <p>{numero_digitado}</p>
-          <p>{divisores.map((val) => `${val} `)}</p>
-          <p>{numeros_primos_divisores.map((val) => `${val} `)}</p>
+        <div className="number-prime-details-container">
+          <p><strong>Numero digitado:</strong> {numero_digitado}</p>
+          <p><strong>Divisores:</strong> {divisores.map((val) => `${val} `)}</p>
+          <p><strong>Divisores Primos:</strong> {numeros_primos_divisores.map((val) => `${val} `)}</p>
         </div>
       ) : null}
     </div>
